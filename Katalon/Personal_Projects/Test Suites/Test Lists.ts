77a8ca58d9b03d2bd8956ff0dd065e95136b314d
lastlink <theefunk@gmail.com>
2018-09-29 23:14:56 -0400

    organize tests suites and remove build

---
+++ Katalon/Personal_Projects/Test Suites/Test Lists.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Lists</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-29T23:07:55</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>16953f3e-89e6-4e1e-b772-c2046d096bbb</testSuiteGuid>
+   <testCaseLink>
+      <guid>b8a949ba-7b16-4acc-8f49-eac115da63dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Tests/Test Lists</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Katalon/Personal_Projects/Test Suites/Test Theme.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Theme</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-29T23:07:54</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d14d088d-73ab-4086-9276-8ce0d197144a</testSuiteGuid>
+   <testCaseLink>
+      <guid>7150691d-ebef-4f64-89d7-ef0a3f212cf0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Tests/Test Theme</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
