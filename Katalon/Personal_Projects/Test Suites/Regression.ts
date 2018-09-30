b89912d774af70d4b00edf5a511fd9ae5433190f
lastlink <theefunk@gmail.com>
2018-09-29 23:58:53 -0400

    include test suite ts

---
+++ Katalon/Personal_Projects/Test Suites/Regression.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-29T23:39:12</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1e0575d2-8915-4f80-b87f-c0a27e5c3103</testSuiteGuid>
+   <testCaseLink>
+      <guid>8cfdc4be-d543-476e-9e20-a38a8d9af9f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/Open Web Application</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ccbc3c16-825e-4656-8f85-4a6c775ab288</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/View Pi Tips</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ef2faff-abba-4709-930c-25ab5172f818</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/View Jenkins Tips</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1deae6ae-b382-46fc-a419-ce9de443802b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/Change Color</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5475968-3089-49e8-9882-12e9e6dfd84c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/Change Theme</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d87c7069-2b52-4909-ab9a-ed7f72c8befb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/Navigate Through lists</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d34d21ab-7094-4fa2-bb06-e062abe6b032</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/View About Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dfd50ae4-467f-436a-9793-217a878dbaca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sub Tests/Close Browser</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Katalon/Personal_Projects/Test Suites/Test Jenkins Tips.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Jenkins Tips</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-09-29T23:07:55</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>33edaa0b-a256-413c-ac07-5ddc96279005</testSuiteGuid>
+   <testCaseLink>
+      <guid>9bdb2e92-e5b1-42c7-93ac-800022291ce9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Tests/Test Jenkins Tips</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
